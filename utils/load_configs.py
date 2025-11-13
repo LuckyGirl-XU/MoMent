b9b37c96297b3246d9427325d87ac0020b29e60a
LuckyGirl-XU <121740199+LuckyGirl-XU@users.noreply.github.com>
2025-11-13 13:09:10 +1100

    Add files via upload

--- utils/load_configs.py
+++ utils/load_configs.py
@@ -11,11 +11,11 @@ def get_link_prediction_args(is_evaluation: bool = False):
     """
     # arguments
     parser = argparse.ArgumentParser('Interface for the link prediction task')
-    parser.add_argument('--dataset_name', type=str, help='dataset to be used', default='Enron',
-                        choices=['Amazon_movies', 'Enron', 'Googlemap_CT', 'ICEWS1819', 'Stack_elec', 'Stack_ubuntu', 'Yelp'])
-    parser.add_argument('--batch_size', type=int, default=256, help='batch size')
-    parser.add_argument('--model_name', type=str, default='GraphMixer', help='name of the model, note that EdgeBank is only applicable for evaluation',
-                        choices=['TGAT','GraphMixer', 'DyGFormer', 'CNEN']) # TGAT,CAWN (ICEWS1819) takes long time; TGN(ICEW1819) OOM
+    parser.add_argument('--dataset_name', type=str, help='dataset to be used', default='Cite_HepTH',
+                        choices=['Amazon_movies', 'Enron', 'GDELT', 'Googlemap_CT', 'ICEWS1819', 'Stack_elec', 'Stack_english', 'Stack_ubuntu', 'Yelp'])
+    parser.add_argument('--batch_size', type=int, default=512, help='batch size')
+    parser.add_argument('--model_name', type=str, default='MoMent', help='name of the model, note that EdgeBank is only applicable for evaluation',
+                        choices=['JODIE', 'DyRep', 'TGAT', 'TGN', 'CAWN', 'EdgeBank', 'TCL', 'GraphMixer', 'DyGFormer', 'CNEN','TimeSGN', 'MoMent', 'TPNet']) # TGAT,CAWN (ICEWS1819) takes long time; TGN(ICEW1819) OOM
     parser.add_argument('--gpu', type=int, default=0, help='number of gpu to use')
     parser.add_argument('--num_neighbors', type=int, default=20, help='number of neighbors to sample for each node')
     parser.add_argument('--sample_neighbor_strategy', type=str, default='recent', choices=['uniform', 'recent', 'time_interval_aware'], help='how to sample historical neighbors')
@@ -36,23 +36,40 @@ def get_link_prediction_args(is_evaluation: bool = False):
     parser.add_argument('--patch_size', type=int, default=1, help='patch size')
     parser.add_argument('--channel_embedding_dim', type=int, default=50, help='dimension of each channel embedding')
     parser.add_argument('--max_input_sequence_length', type=int, default=48, help='maximal length of the input sequence of each node')
-    parser.add_argument('--learning_rate', type=float, default=0.0001, help='learning rate')
+    parser.add_argument('--learning_rate', type=float, default=0.00001, help='learning rate')
     parser.add_argument('--dropout', type=float, default=0.1, help='dropout rate')
-    parser.add_argument('--num_epochs', type=int, default=100, help='number of epochs')
+    parser.add_argument('--num_epochs', type=int, default=50, help='number of epochs')
     parser.add_argument('--optimizer', type=str, default='Adam', choices=['SGD', 'Adam', 'RMSprop'], help='name of optimizer')
     parser.add_argument('--weight_decay', type=float, default=0.0, help='weight decay')
     parser.add_argument('--patience', type=int, default=5, help='patience for early stopping')
     parser.add_argument('--val_ratio', type=float, default=0.15, help='ratio of validation set')
     parser.add_argument('--test_ratio', type=float, default=0.15, help='ratio of test set')
     parser.add_argument('--num_runs', type=int, default=5, help='number of runs')
-    parser.add_argument('--test_interval_epochs', type=int, default=10, help='how many epochs to perform testing once')
+    parser.add_argument('--test_interval_epochs', type=int, default=5, help='how many epochs to perform testing once')
     parser.add_argument('--negative_sample_strategy', type=str, default='random', choices=['random', 'historical', 'inductive'],
                         help='strategy for the negative edge sampling')
     parser.add_argument('--use_feature', type=str, default='', help='whether to use text embeddings as feature') # or Bert
     parser.add_argument('--load_best_configs', action='store_true', default=True, help='whether to load the best configurations')
     parser.add_argument('--memory_dim', type=int, default=64, help='dimension for neighbor hashtable')
     parser.add_argument('--update_neighbor', action='store_true', default=True, help='whether to update neighbor hashtable')
-    parser.add_argument('--loss_weight', type=float, default=0.1, help='Loss weight')
+    parser.add_argument('--loss_weight', type=float, default=0.2, help='Loss weight')
+    parser.add_argument('--use_random_projection', action='store_true', help='whether use the random projection')
+    parser.add_argument('--rp_num_layer', type=int, default=3,
+                        help='the max hop of the maintained temporal walk matrices')
+    parser.add_argument('--rp_time_decay_weight', type=float, default=0.0000001,
+                        help='the time decay weight of the maintained temporal walk matrix')
+    parser.add_argument('--rp_dim_factor', type=int, default=10,
+                        help='the dim factor of random projections w.r.t. the log(2*edge_num)')
+    parser.add_argument('--encode_not_rp', action='store_true', help='whether to use pairwise features in encoder')
+    parser.add_argument('--decode_not_rp', action='store_true', help='whether to use pairwise features in link decoder')
+    parser.add_argument('--rp_not_scale', action='store_true',
+                        help='whether to scale and relu for inner product of random projections')
+    parser.add_argument('--not_encode', action='store_true',
+                        help='whether to use encoder to learn node embeddings from historical interactions')
+    parser.add_argument('--enforce_dim', type=int, default=-1,
+                        help='whether to specific the dimension of random projections')
+    parser.add_argument('--rp_use_matrix', action='store_true',
+                        help='whether to explicitly maintain temporal walk matrices')
 
     try:
         args = parser.parse_args()
@@ -82,11 +99,40 @@ def load_link_prediction_best_configs(args: argparse.Namespace):
         args.num_neighbors = 20
         args.num_layers = 2
         args.sample_neighbor_strategy = 'recent' # 'recent'
+    elif args.model_name in ['JODIE', 'DyRep', 'TGN','TimeSGN']:
+        args.num_neighbors = 10
+        args.num_layers = 1
+        if args.model_name == 'JODIE':
+            args.dropout = 0.1
+        elif args.model_name == 'DyRep':
+            args.dropout = 0.1
+        elif args.model_name == 'TimeSGN':
+            args.num_neighbors = 5
+            args.num_heads = 1
+            args.max_input_sequence_length = 8
+            args.dropout = 0.1
+        else:
+            assert args.model_name == 'TGN'
+            args.num_neighbors = 5
+            args.max_input_sequence_length = 12
+            args.dropout = 0.1
+        if args.model_name in ['TGN', 'DyRep','TimeSGN']:
+            args.sample_neighbor_strategy = 'recent'
+    elif args.model_name == 'CAWN':
+        args.time_scaling_factor = 1e-6
+        args.num_neighbors = 32
+        args.dropout = 0.1
+        args.sample_neighbor_strategy = 'time_interval_aware'
+    elif args.model_name == 'TCL':
+        args.num_neighbors = 20
+        args.num_layers = 2
+        args.dropout = 0.1
+        args.sample_neighbor_strategy = 'recent'
     elif args.model_name == 'GraphMixer':
         args.num_layers = 2
         args.patience = 5
         args.num_neighbors = 20
-        args.dropout = 0.5
+        args.dropout = 0.1
         args.sample_neighbor_strategy = 'recent'
     elif args.model_name == 'DyGFormer':
         args.num_layers = 2
@@ -96,8 +142,33 @@ def load_link_prediction_best_configs(args: argparse.Namespace):
     elif args.model_name == 'CNEN':
         args.num_layers = 2
         args.dropout = 0.1
-        args.max_input_sequence_length = 4
+        args.max_input_sequence_length = 2 #4
         args.memory_dim = 32
+    elif args.model_name == 'TPNet':
+        if args.dataset_name == 'CanParl':
+            args.sample_neighbor_strategy = 'uniform'
+    elif args.model_name == 'MoMent':
+        args.num_layers = 1
+        args.patience = 5
+        args.num_neighbors = 20
+        args.dropout = 0.1
+        args.sample_neighbor_strategy = 'recent'
+        args.num_heads = 2
+        args.learning_rate = 0.00001
+        
+        if args.dataset_name in ['Stack_ubuntu']:
+            
+            args.loss_weight = 0.5
+            
+        elif args.dataset_name in ['Stack_elec']:
+            
+            args.loss_weight = 0.1
+            
+        elif args.dataset_name in ['Amazon_movies', 'Yelp', 'ICEWS1819', 'Googlemap_CT', 'Enron']:
+            
+            args.loss_weight = 0.2
+            
+        
     else:
         raise ValueError(f"Wrong value for model_name {args.model_name}!")
 
@@ -110,10 +181,10 @@ def get_edge_classification_args(is_evaluation: bool = False):
     # arguments
     parser = argparse.ArgumentParser('Interface for the node classification task')
     parser.add_argument('--dataset_name', type=str, help='dataset to be used', default='Enron',
-                        choices=['Amazon_movies', 'Enron', 'Googlemap_CT', 'ICEWS1819', 'Stack_elec', 'Stack_ubuntu', 'Yelp'])
-    parser.add_argument('--batch_size', type=int, default=256, help='batch size') # GDELT, ICEWS1819, 
-    parser.add_argument('--model_name', type=str, default='GraphMixer', help='name of the model',
-                        choices=[ 'TGAT','GraphMixer', 'DyGFormer', 'CNEN'])
+                        choices=['Amazon_movies', 'Enron', 'GDELT', 'Googlemap_CT', 'ICEWS1819', 'Stack_elec', 'Stack_english', 'Stack_ubuntu', 'Yelp'])
+    parser.add_argument('--batch_size', type=int, default=512, help='batch size') # GDELT, ICEWS1819, 
+    parser.add_argument('--model_name', type=str, default='MoMent', help='name of the model',
+                        choices=['JODIE', 'DyRep', 'TGAT', 'TGN', 'CAWN', 'TCL', 'GraphMixer', 'DyGFormer', 'CNEN','TimeSGN', 'MoMent', 'TPNet'])
     parser.add_argument('--gpu', type=int, default=0, help='number of gpu to use')
     parser.add_argument('--num_neighbors', type=int, default=20, help='number of neighbors to sample for each node')
     parser.add_argument('--sample_neighbor_strategy', type=str, default='recent', choices=['uniform', 'recent', 'time_interval_aware'], help='how to sample historical neighbors')
@@ -125,12 +196,12 @@ def get_edge_classification_args(is_evaluation: bool = False):
     parser.add_argument('--num_layers', type=int, default=2, help='number of model layers')
     parser.add_argument('--walk_length', type=int, default=1, help='length of each random walk')
     parser.add_argument('--time_gap', type=int, default=2000, help='time gap for neighbors to compute node features')
-    parser.add_argument('--time_feat_dim', type=int, default=100, help='dimension of the time embedding')
+    parser.add_argument('--time_feat_dim', type=int, default=128, help='dimension of the time embedding')
     parser.add_argument('--position_feat_dim', type=int, default=172, help='dimension of the position embedding')
     parser.add_argument('--patch_size', type=int, default=1, help='patch size')
     parser.add_argument('--channel_embedding_dim', type=int, default=50, help='dimension of each channel embedding')
     parser.add_argument('--max_input_sequence_length', type=int, default=48, help='maximal length of the input sequence of each node')
-    parser.add_argument('--learning_rate', type=float, default=0.0001, help='learning rate')
+    parser.add_argument('--learning_rate', type=float, default=0.00001, help='learning rate')
     parser.add_argument('--dropout', type=float, default=0.1, help='dropout rate')
     parser.add_argument('--num_epochs', type=int, default=100, help='number of epochs')
     parser.add_argument('--optimizer', type=str, default='Adam', choices=['SGD', 'Adam', 'RMSprop'], help='name of optimizer')
@@ -139,13 +210,30 @@ def get_edge_classification_args(is_evaluation: bool = False):
     parser.add_argument('--val_ratio', type=float, default=0.15, help='ratio of validation set')
     parser.add_argument('--test_ratio', type=float, default=0.15, help='ratio of test set')
     parser.add_argument('--num_runs', type=int, default=5, help='number of runs')
-    parser.add_argument('--test_interval_epochs', type=int, default=10, help='how many epochs to perform testing once')
+    parser.add_argument('--test_interval_epochs', type=int, default=5, help='how many epochs to perform testing once')
     parser.add_argument('--use_feature', type=str, default='', help='whether to use text embeddings as feature')
     parser.add_argument('--load_best_configs', action='store_true', default=False, help='whether to load the best configurations')
     parser.add_argument('--update_neighbor', action='store_true', default=True, help='whether to update neighbor hashtable')
     parser.add_argument('--memory_dim', type=int, default=64, help='dimension for neighbor hashtable')
 #     parser.add_argument('--update_neighbor', action='store_true', default=True, help='whether to update neighbor hashtable')
-    parser.add_argument('--loss_weight', type=float, default=0.1, help='Loss weight')
+    parser.add_argument('--loss_weight', type=float, default=0.2, help='Loss weight')
+    parser.add_argument('--use_random_projection', action='store_true', help='whether use the random projection')
+    parser.add_argument('--rp_num_layer', type=int, default=3,
+                        help='the max hop of the maintained temporal walk matrices')
+    parser.add_argument('--rp_time_decay_weight', type=float, default=0.0000001,
+                        help='the time decay weight of the maintained temporal walk matrix')
+    parser.add_argument('--rp_dim_factor', type=int, default=10,
+                        help='the dim factor of random projections w.r.t. the log(2*edge_num)')
+    parser.add_argument('--encode_not_rp', action='store_true', help='whether to use pairwise features in encoder')
+    parser.add_argument('--decode_not_rp', action='store_true', help='whether to use pairwise features in link decoder')
+    parser.add_argument('--rp_not_scale', action='store_true',
+                        help='whether to scale and relu for inner product of random projections')
+    parser.add_argument('--not_encode', action='store_true',
+                        help='whether to use encoder to learn node embeddings from historical interactions')
+    parser.add_argument('--enforce_dim', type=int, default=-1,
+                        help='whether to specific the dimension of random projections')
+    parser.add_argument('--rp_use_matrix', action='store_true',
+                        help='whether to explicitly maintain temporal walk matrices')
    
 
     try:
@@ -176,6 +264,21 @@ def load_edge_classification_best_configs(args: argparse.Namespace):
         args.num_neighbors = 20
         args.num_layers = 2
         args.dropout = 0.1
+    elif args.model_name in ['JODIE', 'DyRep', 'TGN','TimeSGN']:
+        args.num_neighbors = 10
+        args.num_layers = 1
+        args.dropout = 0.1
+        args.num_heads = 1
+        args.sample_neighbor_strategy = 'recent'
+    elif args.model_name == 'CAWN':
+        args.time_scaling_factor = 1e-6
+        args.num_neighbors = 32
+        args.dropout = 0.1
+        args.sample_neighbor_strategy = 'time_interval_aware'
+    elif args.model_name == 'TCL':
+        args.num_neighbors = 20
+        args.num_layers = 2
+        args.dropout = 0.1
     elif args.model_name == 'GraphMixer':
         args.num_layers = 2
         args.dropout = 0.5
@@ -187,8 +290,14 @@ def load_edge_classification_best_configs(args: argparse.Namespace):
     elif args.model_name == 'CNEN':
         args.num_layers = 2
         args.dropout = 0.1
-        args.max_input_sequence_length = 4
+        args.max_input_sequence_length = 2
         args.memory_dim = 32
+    elif args.model_name == 'MoMent':
+        args.num_layers = 1
+        args.patience = 5
+        args.num_neighbors = 20
+        args.dropout = 0.1
+        args.sample_neighbor_strategy = 'recent'
     else:
         raise ValueError(f"Wrong value for model_name {args.model_name}!")
 
